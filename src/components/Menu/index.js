e3929e3f313f7da923d420929aeaa7f23218c0ba
Nelson Jr <juniordell123@gmail.com>
2020-08-01 00:34:17 -0300

    Add eslint

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import Logo from '../../assets/img/logo.png'
-import Button from '../Button'
+import React from "react";
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/logo.png';
+import Button from '../Button';
 
 import './styles.css';
 
 function Menu() {
   return (
-      <nav className='Menu'>
-          <Link to="/">
-            <img className='Logo' src={Logo} alt="NelsonFlix Logo"/>
-          </Link>
+    <nav className='Menu'>
+      <Link to="/">
+        <img className='Logo' src={Logo} alt="NelsonFlix Logo"/>
+      </Link>
 
-          <Button as={Link} className='ButtonLink' to="/cadastro/video">
-              Novo vídeo
+      <Button as={Link} className='ButtonLink' to="/cadastro/video">
+        Novo vídeo
           </Button>
-      </nav>
+    </nav>
   );
 }
 
