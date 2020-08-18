a8eb7ee671c187a9485ab5684ccd2e9e4b794264
Nelson Jr <juniordell123@gmail.com>
2020-08-18 09:58:06 -0300

    Add useForm hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+function useForm(initialValues) {
+    const [values, setValues] = useState(initialValues)
+  
+    function setValue(key, value) {
+      setValues({ ...values, [key]: value })
+    }
+  
+    function handleChange(event) {
+      setValue(event.target.getAttribute('name'), event.target.value)
+    }
+  
+    function clearForm(valoresIniciais) {
+      setValue(valoresIniciais)
+    }
+  
+    return {
+      values,
+      handleChange,
+      clearForm
+    }
+}
+
+export default useForm
